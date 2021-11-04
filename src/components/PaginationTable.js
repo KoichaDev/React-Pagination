7b63f3653caa0f27c85bdea61f75a00fe7e3948b
Khoi Hoang <khoi@godtsagt.no>
2021-11-04 15:45:53 +0100

    Add: ✨ Jump to straight which page we want to see on the pagination

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -33,7 +33,10 @@ const PaginationTable = () => {
         state: {
             pageIndex,  // Displaying page index for incrementing by 1. It will observe where you are at in current pagination number 
         },
-        prepareRow } = tableInstance
+        prepareRow,
+        gotoPage, // Used to indicate which page number we want specific to go at 
+        pageCount, // used for counting how many page left 
+    } = tableInstance
 
     return (
         <>
@@ -65,13 +68,28 @@ const PaginationTable = () => {
                 </tbody>
             </table>
             <div>
-
                 <span>
                     page{' '}
                     <strong>{pageIndex + 1} of {pageOptions.length}</strong>
                 </span> {' '}
+
+                <span>
+                    | Go To Page: {' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(pageNumber)
+                        }}
+                        style={{ width: '50px' }} />
+                </span>
+
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
                 <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
                 <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
+
             </div>
         </>
     )
