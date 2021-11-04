d8042bac5bea860b3576628ea869190787963115
Khoi Hoang <khoi@godtsagt.no>
2021-11-04 14:04:13 +0100

    Add: ✨ Global Filtering
    The Global filtering will filter everything that we type on the input field
    
    Basically, it will filter first name, last name, id, country etc.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BasicTable from './components/BasicTable'
 import SortingTable from './components/SortingTable'
+import FilteringTable from './components/FilteringTable'
 
 const App = () => {
   return (
     <div>
        {/* <BasicTable /> */}
-       <SortingTable />
+       {/* <SortingTable /> */}
+      <FilteringTable />
     </div>
   )
 }
