34800f34f325cb52b8dce1a8f723262ab05ed24e
Khoi Hoang <khoi@godtsagt.no>
2021-11-04 15:26:10 +0100

    Add: ✨ Implemented usePagination hook
    Currently there are no back and forth button to show more pagination,
    but this is a start to show the pagination works pretty fine!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import BasicTable from './components/BasicTable'
 import SortingTable from './components/SortingTable'
 import FilteringTable from './components/FilteringTable'
+import PaginationTable from './components/PaginationTable'
 
 const App = () => {
   return (
     <div>
        {/* <BasicTable /> */}
        {/* <SortingTable /> */}
-      <FilteringTable />
+      {/* <FilteringTable /> */}
+      <PaginationTable />
     </div>
   )
 }
---
+++ src/components/PaginationTable.js
@@ -0,0 +1,60 @@
+import { useMemo } from 'react'
+import { useTable, usePagination } from 'react-table';
+import MOCK_DATA from '../mock-data/MOCK_DATA.json';
+import { COLUMNS, GROUPED_COLUMNS } from './columns'
+import './table.css';
+
+const PaginationTable = () => {
+    // ! The useTable library also recommends to use React use the Memoization-hook (useMemo).
+    // ! If we didn't use memoize the columns and data, React Table would think that we are receiving new data on every render 
+    // ! and then attempt to recalculate it a lot of logic every single time. This will definently affect the component performance 
+    // const columns = useMemo(() => GROUPED_COLUMNS, []);
+    const columns = useMemo(() => COLUMNS, []);
+    const mockData = useMemo(() => MOCK_DATA, []);
+
+    // This hook takes two properties to render the UI. 
+    const tableInstance = useTable({
+        columns: columns,
+        data: mockData
+    }, usePagination)
+
+    // These are functions and arrays from the hook that comes from the useHook package given to us to enable easy table creation
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups, //This will give us access our grouped of header such as first name, last name, id etc.
+        page,
+        prepareRow } = tableInstance
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => {
+                    return <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => {
+                            // The 'Header' property is located on the columns.js file, so basically, id, first_name, last_name etc.
+                            return <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        })}
+                    </tr>
+
+                })}
+            </thead>
+            <tbody {...getTableBodyProps}>
+                {page.map(row => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {
+                                row.cells.map(rowCell => {
+                                    return <td {...rowCell.getCellProps()}>{rowCell.render('Cell')}</td>
+                                })
+                            }
+                        </tr>
+                    )
+                })}
+            </tbody> 
+        </table>
+    )
+}
+
+export default PaginationTable
