5b97c5ded800c2d6d3671b631cfa578d00ff2765
Khoi Hoang <khoi@godtsagt.no>
2021-11-04 12:42:24 +0100

    Add: ✨ Creating table instance by using useTable-hook

---
+++ src/components/BasicTable.js
@@ -0,0 +1,25 @@
+import {useMemo} from 'react'
+import { useTable } from 'react-table';
+import MOCK_DATA from '../mock-data/MOCK_DATA.json';
+import { COLUMNS } from './columns'
+
+const BasicTable = () => {
+    // ! The useTable library also recommends to use React use the Memoization-hook (useMemo).
+    // ! If we didn't use memoize the columns and data, React Table would think that we are receiving new data on every render 
+    // ! and then attempt to recalculate it a lot of logic every single time. This will definently affect the component performance 
+    const columns = useMemo(() => COLUMNS, []);
+    const mockData =useMemo(() => MOCK_DATA, []);
+
+    // This hook takes two properties to render the UI. 
+    const tableInstance =useTable({
+        columns: columns,
+        data: mockData
+    })
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BasicTable
